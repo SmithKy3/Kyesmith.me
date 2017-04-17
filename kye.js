30ae60b1a5d5909a0dd8b84dea046b91382d83f8
Kye <KLJSmith1@sheffield.ac.uk>
2017-04-17 18:16:49 +0100

    Changed home page, more changes coming soon

--- kye.js
+++ kye.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 $(function() {
 	$('a[href*=#]').on('click', function(e) {
 		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
 	});
 });
 });
