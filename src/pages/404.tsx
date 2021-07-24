dcdc941034a9c289dd03511f0dbbc0bfeb833602
Kye Smith <kye@m3ter.com>
2021-07-24 20:57:04 +0100

    Update warpspeed version and fix 404 bug

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,39 +1,42 @@
-import React, { createRef, useEffect, useMemo } from 'react';
-import { NextLayoutPage } from 'next';
-import Head from 'next/head';
-import { getWarpSpeedController } from 'warpspeed';
+import React, { createRef, useEffect, useRef } from "react";
+import { NextLayoutPage } from "next";
+import Head from "next/head";
+import { getWarpSpeedController, WarpSpeedController } from "warpspeed";
 
-import { DefaultLayout } from 'components/layouts/Default';
-import * as ThorOhThorSections from 'components/page-sections/ThorOhThor';
+import { DefaultLayout } from "components/layouts/Default";
+import * as ThorOhThorSections from "components/page-sections/ThorOhThor";
 
 const FourOFour: NextLayoutPage = () => {
-    const canvasContainerRef = createRef<HTMLDivElement>();
-    const warpspeedController = useMemo(() => getWarpSpeedController(), []);
+  const canvasContainerRef = createRef<HTMLDivElement>();
+  const warpspeedController = useRef<WarpSpeedController>();
 
-    useEffect(() => {
-        if (canvasContainerRef.current) {
-            warpspeedController.mountCanvasTo(canvasContainerRef.current);
-            warpspeedController.render();
+  useEffect(() => {
+    if (canvasContainerRef.current && typeof window != "undefined") {
+      warpspeedController.current = getWarpSpeedController();
+      warpspeedController.current.mountCanvasTo(canvasContainerRef.current);
+      warpspeedController.current.render();
 
-            return () => warpspeedController.dismountCanvas();
-        }
-    }, [warpspeedController, canvasContainerRef]);
+      return () => warpspeedController.current?.dismountCanvas();
+    }
+  }, [warpspeedController, canvasContainerRef]);
 
-    return (
-        <>
-            <Head>
-                <title>This is not the page you're looking for...</title>
-                <meta name="keywords" content={'kye,smith,404'}/>
-            </Head>
-            <ThorOhThorSections.CanvasContainer
-                ref={canvasContainerRef}
-                onClick={() => window.location.href = '/'}
-            >
-                <ThorOhThorSections.Text>Looks like you're lost... click this to go back</ThorOhThorSections.Text>
-            </ThorOhThorSections.CanvasContainer>
-        </>
-    );
-}
+  return (
+    <>
+      <Head>
+        <title>This is not the page you're looking for...</title>
+        <meta name="keywords" content={"kye,smith,404"} />
+      </Head>
+      <ThorOhThorSections.CanvasContainer
+        ref={canvasContainerRef}
+        onClick={() => (window.location.href = "/")}
+      >
+        <ThorOhThorSections.Text>
+          Looks like you're lost... click this to go back
+        </ThorOhThorSections.Text>
+      </ThorOhThorSections.CanvasContainer>
+    </>
+  );
+};
 
-FourOFour.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>
+FourOFour.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>;
 export default FourOFour;
